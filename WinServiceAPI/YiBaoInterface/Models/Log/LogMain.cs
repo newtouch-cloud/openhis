38fcf07ac879f4c3227102c1d574204b24fdead4
bingjian.wang@newtouch.com <bingjian.wang@newtouch.com>
2024-08-24 12:17:53 +0800

    医保的依赖修改

---
+++ WinServiceAPI/YiBaoInterface/Models/Log/LogMain.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NeiMengGuYiBaoApp.Models.Log
+{
+    public class LogMain
+    {
+        /// <summary>
+        /// 凭证号码
+        /// </summary>
+        public decimal inNumber { get; set; }
+        /// <summary>
+        /// 输入日期
+        /// </summary>
+        public DateTime inDate { get; set; }
+        /// <summary>
+        /// 交易编码
+        /// </summary>
+        public string tradiNumber { get; set; }
+
+        /// <summary>
+        /// 用户电脑ip
+        /// </summary>
+        public string userIp { get; set; }
+        /// <summary>
+        /// 操作员id
+        /// </summary>
+        public string operatorId { get; set; }
+
+
+    }
+}
