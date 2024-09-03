f8a329bac6f89547b29c8063f2689aeeb2ece342
bingjian.wang@newtouch.com <bingjian.wang@newtouch.com>
2024-09-03 18:13:08 +0800

    进销存的查询

---
+++ Newtouch.HIS.Sett/Newtouch.Domain/IDomainServices/DRGManage/IDRGUploadDmnService.cs
@@ -0,0 +1,16 @@
+﻿using Newtouch.Core.Common;
+using Newtouch.HIS.Domain.DTO.OutputDto.DRGManage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Newtouch.HIS.Domain.IDomainServices.DRGManage
+{
+    public interface IDRGUploadDmnService
+    {
+        List<DRGUploadDto> GetList(Pagination pagination, string orgId, string kssj, string jssj,string scqk,string zyh);
+        int DRGUpload(string orgId,List<DRGUploadDto> list);
+    }
+}
