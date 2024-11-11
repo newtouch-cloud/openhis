a6a5f1f902681a591ff6e992625e64377189264d
tyz <yunzheng.tan@newtouch.com>
2024-11-11 11:48:26 +0800

    电子处方目录筛选

--- Newtouch.HIS.Base/Newtouch.HIS.DomainServices/PharmacyDrugStorage/SysMedicineElectronicPrescriptionDmnService.cs
+++ Newtouch.HIS.Base/Newtouch.HIS.DomainServices/PharmacyDrugStorage/SysMedicineElectronicPrescriptionDmnService.cs
@@ -24,7 +24,7 @@ namespace Newtouch.HIS.DomainServices.PharmacyDrugStorage
         /// 获取当前组织下的系统药品信息
         /// </summary>
         /// <returns></returns>
-        public IList<SysMedicineElectronicPrescriptionVO> GetPaginationList(Pagination pagination, string genname, string medListCodg)
+        public IList<SysMedicineElectronicPrescriptionVO> GetPaginationList(Pagination pagination, string genname, string medListCodg, string listType, string zt)
         {
             var sql = @"select * from [NewtouchHIS_Sett]..Dzcf_CFYP_output where 1=1 ";
             if (!string.IsNullOrEmpty(genname))
@@ -33,13 +33,23 @@ namespace Newtouch.HIS.DomainServices.PharmacyDrugStorage
             }
             if (!string.IsNullOrEmpty(medListCodg))
             {
-                sql += @" and medListCodg=@medListCodg";
+                sql += @" and medListCodg like @medListCodg";
+            }
+            if (!string.IsNullOrWhiteSpace(listType))
+            {
+                sql += @" and listType like @listType";
+            }
+            if (!string.IsNullOrWhiteSpace(zt))
+            {
+                sql += @" and zt like @zt";
             }
             DbParameter[] par = new DbParameter[]
-                {
-                    new SqlParameter("@genname", "%"+genname.Trim()+"%"),
-                    new SqlParameter("@medListCodg", medListCodg),
-                };
+            {
+                new SqlParameter("@genname", "%"+genname.Trim()+"%"),
+                new SqlParameter("@medListCodg", "%"+medListCodg+"%"),
+                new SqlParameter("@listType", "%"+listType.Trim()+"%"),
+                new SqlParameter("@zt", "%"+zt+"%"),
+            };
             return this.QueryWithPage<SysMedicineElectronicPrescriptionVO>(sql, pagination,par
             );
         }
