6200750876a4add1d4e66266feff0143a3560626
abing <410174833@qq.com>
2025-03-05 17:21:43 +0800

    修改配置sql

--- SqlServer/V1.8.2/update_Sys_ConfigTemplate.sql
+++ SqlServer/V1.8.2/update_Sys_ConfigTemplate.sql
@@ -1,246 +1,336 @@
 -----------------------------------
-INSERT INTO [NewtouchHIS_Sett].[dbo].[Sys_ConfigTemplate]
-    ([Id], [Code], [Name], [Memo], [CreateTime], [CreatorCode], [LastModifyTime], [LastModifierCode], [zt], [px], [DefaultVal])
-SELECT 
-    NEWID() AS [Id],
-    sc.[Code],
-    sc.[Name],
-    sc.[Memo],
-    sc.[CreateTime],
-    sc.[CreatorCode],
-    sc.[LastModifyTime],
-    sc.[LastModifierCode],
-    sc.[zt],
-    sc.[px],
-    sc.Value AS [DefaultVal]
-FROM 
-    [NewtouchHIS_Sett].[dbo].[Sys_Config] sc
-LEFT JOIN 
-    [NewtouchHIS_Sett].[dbo].[Sys_ConfigTemplate] sct
-    ON sc.[Code] = sct.[Code]
-WHERE 
-    sc.[OrganizeId] = '6d5752a7-234a-403e-aa1c-df8b45d3469f'
-    AND sct.[Code] IS NULL;
-
+MERGE INTO [NewtouchHIS_Sett].[dbo].[Sys_ConfigTemplate] AS target
+USING (
+    SELECT 
+        NEWID() AS [Id], -- 新增时生成新 ID
+        sc.[Code],
+        sc.[Name],
+        sc.[Memo],
+        sc.[CreateTime],  -- 用当前时间
+        sc.[CreatorCode],
+        sc. [LastModifyTime],  -- 用当前时间
+        sc.[LastModifierCode],
+        sc.[zt],
+        sc.[px],
+        sc.[Value] AS [DefaultVal]
+    FROM [NewtouchHIS_Sett].[dbo].[Sys_Config] sc
+    WHERE sc.[OrganizeId] = '6d5752a7-234a-403e-aa1c-df8b45d3469f'
+) AS source
+ON target.[Code] = source.[Code]  -- 以 Code 作为匹配条件
+WHEN MATCHED THEN 
+    -- **如果 Code 存在，更新数据**
+    UPDATE SET 
+        target.[Name] = source.[Name],
+        target.[Memo] = source.[Memo],
+        target.[LastModifyTime] = source.[LastModifyTime], 
+        target.[LastModifierCode] = source.[LastModifierCode],
+        target.[zt] = source.[zt],
+        target.[px] = source.[px],
+        target.[DefaultVal] = source.[DefaultVal]
+WHEN NOT MATCHED THEN 
+    -- **如果 Code 不存在，插入数据**
+    INSERT ([Id], [Code], [Name], [Memo], [CreateTime], [CreatorCode], 
+            [LastModifyTime], [LastModifierCode], [zt], [px], [DefaultVal])
+    VALUES (source.[Id], source.[Code], source.[Name], source.[Memo], 
+            source.[CreateTime], source.[CreatorCode], source.[LastModifyTime], 
+            source.[LastModifierCode], source.[zt], source.[px], source.[DefaultVal]);
 
 --------------------------------
-INSERT INTO [Newtouch_CIS].[dbo].[Sys_ConfigTemplate]
-    ([Id], [Code], [Name], [Memo], [CreateTime], [CreatorCode], [LastModifyTime], [LastModifierCode], [zt], [px], [DefaultVal])
-SELECT 
-    NEWID() AS [Id],
-    sc.[Code],
-    sc.[Name],
-    sc.[Memo],
-    sc.[CreateTime],
-    sc.[CreatorCode],
-    sc.[LastModifyTime],
-    sc.[LastModifierCode],
-    sc.[zt],
-    sc.[px],
-    sc.Value AS [DefaultVal]
-FROM 
-    [Newtouch_CIS].[dbo].[Sys_Config] sc
-LEFT JOIN 
-    [Newtouch_CIS].[dbo].[Sys_ConfigTemplate] sct
-    ON sc.[Code] = sct.[Code]
-WHERE 
-    sc.[OrganizeId] = '6d5752a7-234a-403e-aa1c-df8b45d3469f'
-    AND sct.[Code] IS NULL;
-
+MERGE INTO [Newtouch_CIS].[dbo].[Sys_ConfigTemplate] AS target
+USING (
+    SELECT 
+        NEWID() AS [Id], -- 新增时生成新 ID
+        sc.[Code],
+        sc.[Name],
+        sc.[Memo],
+        sc.[CreateTime],  -- 用当前时间
+        sc.[CreatorCode],
+        sc. [LastModifyTime],  -- 用当前时间
+        sc.[LastModifierCode],
+        sc.[zt],
+        sc.[px],
+        sc.[Value] AS [DefaultVal]
+    FROM [Newtouch_CIS].[dbo].[Sys_Config] sc
+    WHERE sc.[OrganizeId] = '6d5752a7-234a-403e-aa1c-df8b45d3469f'
+) AS source
+ON target.[Code] = source.[Code]  -- 以 Code 作为匹配条件
+WHEN MATCHED THEN 
+    -- **如果 Code 存在，更新数据**
+    UPDATE SET 
+        target.[Name] = source.[Name],
+        target.[Memo] = source.[Memo],
+        target.[LastModifyTime] = source.[LastModifyTime], 
+        target.[LastModifierCode] = source.[LastModifierCode],
+        target.[zt] = source.[zt],
+        target.[px] = source.[px],
+        target.[DefaultVal] = source.[DefaultVal]
+WHEN NOT MATCHED THEN 
+    -- **如果 Code 不存在，插入数据**
+    INSERT ([Id], [Code], [Name], [Memo], [CreateTime], [CreatorCode], 
+            [LastModifyTime], [LastModifierCode], [zt], [px], [DefaultVal])
+    VALUES (source.[Id], source.[Code], source.[Name], source.[Memo], 
+            source.[CreateTime], source.[CreatorCode], source.[LastModifyTime], 
+            source.[LastModifierCode], source.[zt], source.[px], source.[DefaultVal]);
 
 ----------------------------------------
-INSERT INTO [NewtouchHIS_PDS].[dbo].[Sys_ConfigTemplate]
-    ([Id], [Code], [Name], [Memo], [CreateTime], [CreatorCode], [LastModifyTime], [LastModifierCode], [zt], [px], [DefaultVal])
-SELECT 
-    NEWID() AS [Id],
-    sc.[Code],
-    sc.[Name],
-    sc.[Memo],
-    sc.[CreateTime],
-    sc.[CreatorCode],
-    sc.[LastModifyTime],
-    sc.[LastModifierCode],
-    sc.[zt],
-    sc.[px],
-    sc.Value AS [DefaultVal]
-FROM 
-    [NewtouchHIS_PDS].[dbo].[Sys_Config] sc
-LEFT JOIN 
-    [NewtouchHIS_PDS].[dbo].[Sys_ConfigTemplate] sct
-    ON sc.[Code] = sct.[Code]
-WHERE 
-    sc.[OrganizeId] = '6d5752a7-234a-403e-aa1c-df8b45d3469f'
-    AND sct.[Code] IS NULL;
+MERGE INTO [NewtouchHIS_PDS].[dbo].[Sys_ConfigTemplate] AS target
+USING (
+    SELECT 
+        NEWID() AS [Id], -- 新增时生成新 ID
+        sc.[Code],
+        sc.[Name],
+        sc.[Memo],
+        sc.[CreateTime],  -- 用当前时间
+        sc.[CreatorCode],
+        sc. [LastModifyTime],  -- 用当前时间
+        sc.[LastModifierCode],
+        sc.[zt],
+        sc.[px],
+        sc.[Value] AS [DefaultVal]
+    FROM [NewtouchHIS_PDS].[dbo].[Sys_Config] sc
+    WHERE sc.[OrganizeId] = '6d5752a7-234a-403e-aa1c-df8b45d3469f'
+) AS source
+ON target.[Code] = source.[Code]  -- 以 Code 作为匹配条件
+WHEN MATCHED THEN 
+    -- **如果 Code 存在，更新数据**
+    UPDATE SET 
+        target.[Name] = source.[Name],
+        target.[Memo] = source.[Memo],
+        target.[LastModifyTime] = source.[LastModifyTime], 
+        target.[LastModifierCode] = source.[LastModifierCode],
+        target.[zt] = source.[zt],
+        target.[px] = source.[px],
+        target.[DefaultVal] = source.[DefaultVal]
+WHEN NOT MATCHED THEN 
+    -- **如果 Code 不存在，插入数据**
+    INSERT ([Id], [Code], [Name], [Memo], [CreateTime], [CreatorCode], 
+            [LastModifyTime], [LastModifierCode], [zt], [px], [DefaultVal])
+    VALUES (source.[Id], source.[Code], source.[Name], source.[Memo], 
+            source.[CreateTime], source.[CreatorCode], source.[LastModifyTime], 
+            source.[LastModifierCode], source.[zt], source.[px], source.[DefaultVal]);
 
-INSERT INTO [Newtouch_EMR].[dbo].[Sys_ConfigTemplate]
-    ([Id], [Code], [Name], [Memo], [CreateTime], [CreatorCode], [LastModifyTime], [LastModifierCode], [zt], [px], [DefaultVal])
-SELECT 
-    NEWID() AS [Id],
-    sc.[Code],
-    sc.[Name],
-    sc.[Memo],
-    sc.[CreateTime],
-    sc.[CreatorCode],
-    sc.[LastModifyTime],
-    sc.[LastModifierCode],
-    sc.[zt],
-    sc.[px],
-    sc.Value AS [DefaultVal]
-FROM 
-    [Newtouch_EMR].[dbo].[Sys_Config] sc
-LEFT JOIN 
-    [Newtouch_EMR].[dbo].[Sys_ConfigTemplate] sct
-    ON sc.[Code] = sct.[Code]
-WHERE 
-    sc.[OrganizeId] = '6d5752a7-234a-403e-aa1c-df8b45d3469f'
-    AND sct.[Code] IS NULL;
+----------------------------------------
+MERGE INTO [Newtouch_EMR].[dbo].[Sys_ConfigTemplate] AS target
+USING (
+    SELECT 
+        NEWID() AS [Id], -- 新增时生成新 ID
+        sc.[Code],
+        sc.[Name],
+        sc.[Memo],
+        sc.[CreateTime],  -- 用当前时间
+        sc.[CreatorCode],
+        sc. [LastModifyTime],  -- 用当前时间
+        sc.[LastModifierCode],
+        sc.[zt],
+        sc.[px],
+        sc.[Value] AS [DefaultVal]
+    FROM [Newtouch_EMR].[dbo].[Sys_Config] sc
+    WHERE sc.[OrganizeId] = '6d5752a7-234a-403e-aa1c-df8b45d3469f'
+) AS source
+ON target.[Code] = source.[Code]  -- 以 Code 作为匹配条件
+WHEN MATCHED THEN 
+    -- **如果 Code 存在，更新数据**
+    UPDATE SET 
+        target.[Name] = source.[Name],
+        target.[Memo] = source.[Memo],
+        target.[LastModifyTime] = source.[LastModifyTime], 
+        target.[LastModifierCode] = source.[LastModifierCode],
+        target.[zt] = source.[zt],
+        target.[px] = source.[px],
+        target.[DefaultVal] = source.[DefaultVal]
+WHEN NOT MATCHED THEN 
+    -- **如果 Code 不存在，插入数据**
+    INSERT ([Id], [Code], [Name], [Memo], [CreateTime], [CreatorCode], 
+            [LastModifyTime], [LastModifierCode], [zt], [px], [DefaultVal])
+    VALUES (source.[Id], source.[Code], source.[Name], source.[Memo], 
+            source.[CreateTime], source.[CreatorCode], source.[LastModifyTime], 
+            source.[LastModifierCode], source.[zt], source.[px], source.[DefaultVal]);
 
 
 -----------------------------------
-INSERT INTO [Newtouch_EMR].[dbo].[Sys_ConfigTemplate]
-    ([Id], [Code], [Name], [Memo], [CreateTime], [CreatorCode], [LastModifyTime], [LastModifierCode], [zt], [px], [DefaultVal])
-SELECT 
-    NEWID() AS [Id],
-    sc.[Code],
-    sc.[Name],
-    sc.[Memo],
-    sc.[CreateTime],
-    sc.[CreatorCode],
-    sc.[LastModifyTime],
-    sc.[LastModifierCode],
-    sc.[zt],
-    sc.[px],
-    sc.Value AS [DefaultVal]
-FROM 
-    [Newtouch_EMR].[dbo].[Sys_Config] sc
-LEFT JOIN 
-    [Newtouch_EMR].[dbo].[Sys_ConfigTemplate] sct
-    ON sc.[Code] = sct.[Code]
-WHERE 
-    sc.[OrganizeId] = '6d5752a7-234a-403e-aa1c-df8b45d3469f'
-    AND sct.[Code] IS NULL;
-
-
------------------------------
-INSERT INTO [Newtouch_MRMS].[dbo].[Sys_ConfigTemplate]
-    ([Id], [Code], [Name], [Memo], [CreateTime], [CreatorCode], [LastModifyTime], [LastModifierCode], [zt], [px], [DefaultVal])
-SELECT 
-    NEWID() AS [Id],
-    sc.[Code],
-    sc.[Name],
-    sc.[Memo],
-    sc.[CreateTime],
-    sc.[CreatorCode],
-    sc.[LastModifyTime],
-    sc.[LastModifierCode],
-    sc.[zt],
-    sc.[px],
-    sc.Value AS [DefaultVal]
-FROM 
-    [Newtouch_MRMS].[dbo].[Sys_Config] sc
-LEFT JOIN 
-    [Newtouch_MRMS].[dbo].[Sys_ConfigTemplate] sct
-    ON sc.[Code] = sct.[Code]
-WHERE 
-    sc.[OrganizeId] = '6d5752a7-234a-403e-aa1c-df8b45d3469f'
-    AND sct.[Code] IS NULL;
+MERGE INTO [Newtouch_MRMS].[dbo].[Sys_ConfigTemplate] AS target
+USING (
+    SELECT 
+        NEWID() AS [Id], -- 新增时生成新 ID
+        sc.[Code],
+        sc.[Name],
+        sc.[Memo],
+        sc.[CreateTime],  -- 用当前时间
+        sc.[CreatorCode],
+        sc. [LastModifyTime],  -- 用当前时间
+        sc.[LastModifierCode],
+        sc.[zt],
+        sc.[px],
+        sc.[Value] AS [DefaultVal]
+    FROM [Newtouch_MRMS].[dbo].[Sys_Config] sc
+    WHERE sc.[OrganizeId] = '6d5752a7-234a-403e-aa1c-df8b45d3469f'
+) AS source
+ON target.[Code] = source.[Code]  -- 以 Code 作为匹配条件
+WHEN MATCHED THEN 
+    -- **如果 Code 存在，更新数据**
+    UPDATE SET 
+        target.[Name] = source.[Name],
+        target.[Memo] = source.[Memo],
+        target.[LastModifyTime] = source.[LastModifyTime], 
+        target.[LastModifierCode] = source.[LastModifierCode],
+        target.[zt] = source.[zt],
+        target.[px] = source.[px],
+        target.[DefaultVal] = source.[DefaultVal]
+WHEN NOT MATCHED THEN 
+    -- **如果 Code 不存在，插入数据**
+    INSERT ([Id], [Code], [Name], [Memo], [CreateTime], [CreatorCode], 
+            [LastModifyTime], [LastModifierCode], [zt], [px], [DefaultVal])
+    VALUES (source.[Id], source.[Code], source.[Name], source.[Memo], 
+            source.[CreateTime], source.[CreatorCode], source.[LastModifyTime], 
+            source.[LastModifierCode], source.[zt], source.[px], source.[DefaultVal]);
 
 
 -------------------------------
-INSERT INTO [Newtouch_MRQC].[dbo].[Sys_ConfigTemplate]
-    ([Id], [Code], [Name], [Memo], [CreateTime], [CreatorCode], [LastModifyTime], [LastModifierCode], [zt], [px], [DefaultVal])
-SELECT 
-    NEWID() AS [Id],
-    sc.[Code],
-    sc.[Name],
-    sc.[Memo],
-    sc.[CreateTime],
-    sc.[CreatorCode],
-    sc.[LastModifyTime],
-    sc.[LastModifierCode],
-    sc.[zt],
-    sc.[px],
-    sc.Value AS [DefaultVal]
-FROM 
-    [Newtouch_MRQC].[dbo].[Sys_Config] sc
-LEFT JOIN 
-    [Newtouch_MRQC].[dbo].[Sys_ConfigTemplate] sct
-    ON sc.[Code] = sct.[Code]
-WHERE 
-    sc.[OrganizeId] = '6d5752a7-234a-403e-aa1c-df8b45d3469f'
-    AND sct.[Code] IS NULL;
+MERGE INTO [Newtouch_MRQC].[dbo].[Sys_ConfigTemplate] AS target
+USING (
+    SELECT 
+        NEWID() AS [Id], -- 新增时生成新 ID
+        sc.[Code],
+        sc.[Name],
+        sc.[Memo],
+        sc.[CreateTime],  -- 用当前时间
+        sc.[CreatorCode],
+        sc. [LastModifyTime],  -- 用当前时间
+        sc.[LastModifierCode],
+        sc.[zt],
+        sc.[px],
+        sc.[Value] AS [DefaultVal]
+    FROM [Newtouch_MRQC].[dbo].[Sys_Config] sc
+    WHERE sc.[OrganizeId] = '6d5752a7-234a-403e-aa1c-df8b45d3469f'
+) AS source
+ON target.[Code] = source.[Code]  -- 以 Code 作为匹配条件
+WHEN MATCHED THEN 
+    -- **如果 Code 存在，更新数据**
+    UPDATE SET 
+        target.[Name] = source.[Name],
+        target.[Memo] = source.[Memo],
+        target.[LastModifyTime] = source.[LastModifyTime], 
+        target.[LastModifierCode] = source.[LastModifierCode],
+        target.[zt] = source.[zt],
+        target.[px] = source.[px],
+        target.[DefaultVal] = source.[DefaultVal]
+WHEN NOT MATCHED THEN 
+    -- **如果 Code 不存在，插入数据**
+    INSERT ([Id], [Code], [Name], [Memo], [CreateTime], [CreatorCode], 
+            [LastModifyTime], [LastModifierCode], [zt], [px], [DefaultVal])
+    VALUES (source.[Id], source.[Code], source.[Name], source.[Memo], 
+            source.[CreateTime], source.[CreatorCode], source.[LastModifyTime], 
+            source.[LastModifierCode], source.[zt], source.[px], source.[DefaultVal]);
 
 
 ------------------------------------------
-INSERT INTO [Newtouch_OR].[dbo].[Sys_ConfigTemplate]
-    ([Id], [Code], [Name], [Memo], [CreateTime], [CreatorCode], [LastModifyTime], [LastModifierCode], [zt], [px], [DefaultVal])
-SELECT 
-    NEWID() AS [Id],
-    sc.[Code],
-    sc.[Name],
-    sc.[Memo],
-    sc.[CreateTime],
-    sc.[CreatorCode],
-    sc.[LastModifyTime],
-    sc.[LastModifierCode],
-    sc.[zt],
-    sc.[px],
-    sc.Value AS [DefaultVal]
-FROM 
-    [Newtouch_OR].[dbo].[Sys_Config] sc
-LEFT JOIN 
-    [Newtouch_OR].[dbo].[Sys_ConfigTemplate] sct
-    ON sc.[Code] = sct.[Code]
-WHERE 
-    sc.[OrganizeId] = '6d5752a7-234a-403e-aa1c-df8b45d3469f'
-    AND sct.[Code] IS NULL;
+MERGE INTO [Newtouch_OR].[dbo].[Sys_ConfigTemplate] AS target
+USING (
+    SELECT 
+        NEWID() AS [Id], -- 新增时生成新 ID
+        sc.[Code],
+        sc.[Name],
+        sc.[Memo],
+        sc.[CreateTime],  -- 用当前时间
+        sc.[CreatorCode],
+        sc. [LastModifyTime],  -- 用当前时间
+        sc.[LastModifierCode],
+        sc.[zt],
+        sc.[px],
+        sc.[Value] AS [DefaultVal]
+    FROM [Newtouch_OR].[dbo].[Sys_Config] sc
+    WHERE sc.[OrganizeId] = '6d5752a7-234a-403e-aa1c-df8b45d3469f'
+) AS source
+ON target.[Code] = source.[Code]  -- 以 Code 作为匹配条件
+WHEN MATCHED THEN 
+    -- **如果 Code 存在，更新数据**
+    UPDATE SET 
+        target.[Name] = source.[Name],
+        target.[Memo] = source.[Memo],
+        target.[LastModifyTime] = source.[LastModifyTime], 
+        target.[LastModifierCode] = source.[LastModifierCode],
+        target.[zt] = source.[zt],
+        target.[px] = source.[px],
+        target.[DefaultVal] = source.[DefaultVal]
+WHEN NOT MATCHED THEN 
+    -- **如果 Code 不存在，插入数据**
+    INSERT ([Id], [Code], [Name], [Memo], [CreateTime], [CreatorCode], 
+            [LastModifyTime], [LastModifierCode], [zt], [px], [DefaultVal])
+    VALUES (source.[Id], source.[Code], source.[Name], source.[Memo], 
+            source.[CreateTime], source.[CreatorCode], source.[LastModifyTime], 
+            source.[LastModifierCode], source.[zt], source.[px], source.[DefaultVal]);
 
 
 -----------------------------------------------
-INSERT INTO [Newtouch_Union].[dbo].[Sys_ConfigTemplate]
-    ([Id], [Code], [Name], [Memo], [CreateTime], [CreatorCode], [LastModifyTime], [LastModifierCode], [zt], [px], [DefaultVal])
-SELECT 
-    NEWID() AS [Id],
-    sc.[Code],
-    sc.[Name],
-    sc.[Memo],
-    sc.[CreateTime],
-    sc.[CreatorCode],
-    sc.[LastModifyTime],
-    sc.[LastModifierCode],
-    sc.[zt],
-    sc.[px],
-    sc.Value AS [DefaultVal]
-FROM 
-    [Newtouch_Union].[dbo].[Sys_Config] sc
-LEFT JOIN 
-    [Newtouch_Union].[dbo].[Sys_ConfigTemplate] sct
-    ON sc.[Code] = sct.[Code]
-WHERE 
-    sc.[OrganizeId] = '6d5752a7-234a-403e-aa1c-df8b45d3469f'
-    AND sct.[Code] IS NULL;
-
+MERGE INTO [Newtouch_Union].[dbo].[Sys_ConfigTemplate] AS target
+USING (
+    SELECT 
+        NEWID() AS [Id], -- 新增时生成新 ID
+        sc.[Code],
+        sc.[Name],
+        sc.[Memo],
+        sc.[CreateTime],  -- 用当前时间
+        sc.[CreatorCode],
+        sc. [LastModifyTime],  -- 用当前时间
+        sc.[LastModifierCode],
+        sc.[zt],
+        sc.[px],
+        sc.[Value] AS [DefaultVal]
+    FROM [Newtouch_Union].[dbo].[Sys_Config] sc
+    WHERE sc.[OrganizeId] = '6d5752a7-234a-403e-aa1c-df8b45d3469f'
+) AS source
+ON target.[Code] = source.[Code]  -- 以 Code 作为匹配条件
+WHEN MATCHED THEN 
+    -- **如果 Code 存在，更新数据**
+    UPDATE SET 
+        target.[Name] = source.[Name],
+        target.[Memo] = source.[Memo],
+        target.[LastModifyTime] = source.[LastModifyTime], 
+        target.[LastModifierCode] = source.[LastModifierCode],
+        target.[zt] = source.[zt],
+        target.[px] = source.[px],
+        target.[DefaultVal] = source.[DefaultVal]
+WHEN NOT MATCHED THEN 
+    -- **如果 Code 不存在，插入数据**
+    INSERT ([Id], [Code], [Name], [Memo], [CreateTime], [CreatorCode], 
+            [LastModifyTime], [LastModifierCode], [zt], [px], [DefaultVal])
+    VALUES (source.[Id], source.[Code], source.[Name], source.[Memo], 
+            source.[CreateTime], source.[CreatorCode], source.[LastModifyTime], 
+            source.[LastModifierCode], source.[zt], source.[px], source.[DefaultVal]);
 
 ----------------------------------------------
-INSERT INTO [NewtouchHIS_herp].[dbo].[Sys_ConfigTemplate]
-    ([Id], [Code], [Name], [Memo], [CreateTime], [CreatorCode], [LastModifyTime], [LastModifierCode], [zt], [px], [DefaultVal])
-SELECT 
-    NEWID() AS [Id],
-    sc.[Code],
-    sc.[Name],
-    sc.[Memo],
-    sc.[CreateTime],
-    sc.[CreatorCode],
-    sc.[LastModifyTime],
-    sc.[LastModifierCode],
-    sc.[zt],
-    sc.[px],
-    sc.Value AS [DefaultVal]
-FROM 
-    [NewtouchHIS_herp].[dbo].[Sys_Config] sc
-LEFT JOIN 
-    [NewtouchHIS_herp].[dbo].[Sys_ConfigTemplate] sct
-    ON sc.[Code] = sct.[Code]
-WHERE 
-    sc.[OrganizeId] = '6d5752a7-234a-403e-aa1c-df8b45d3469f'
-    AND sct.[Code] IS NULL;
+MERGE INTO [NewtouchHIS_herp].[dbo].[Sys_ConfigTemplate] AS target
+USING (
+    SELECT 
+        NEWID() AS [Id], -- 新增时生成新 ID
+        sc.[Code],
+        sc.[Name],
+        sc.[Memo],
+        sc.[CreateTime],  -- 用当前时间
+        sc.[CreatorCode],
+        sc. [LastModifyTime],  -- 用当前时间
+        sc.[LastModifierCode],
+        sc.[zt],
+        sc.[px],
+        sc.[Value] AS [DefaultVal]
+    FROM [NewtouchHIS_herp].[dbo].[Sys_Config] sc
+    WHERE sc.[OrganizeId] = '6d5752a7-234a-403e-aa1c-df8b45d3469f'
+) AS source
+ON target.[Code] = source.[Code]  -- 以 Code 作为匹配条件
+WHEN MATCHED THEN 
+    -- **如果 Code 存在，更新数据**
+    UPDATE SET 
+        target.[Name] = source.[Name],
+        target.[Memo] = source.[Memo],
+        target.[LastModifyTime] = source.[LastModifyTime], 
+        target.[LastModifierCode] = source.[LastModifierCode],
+        target.[zt] = source.[zt],
+        target.[px] = source.[px],
+        target.[DefaultVal] = source.[DefaultVal]
+WHEN NOT MATCHED THEN 
+    -- **如果 Code 不存在，插入数据**
+    INSERT ([Id], [Code], [Name], [Memo], [CreateTime], [CreatorCode], 
+            [LastModifyTime], [LastModifierCode], [zt], [px], [DefaultVal])
+    VALUES (source.[Id], source.[Code], source.[Name], source.[Memo], 
+            source.[CreateTime], source.[CreatorCode], source.[LastModifyTime], 
+            source.[LastModifierCode], source.[zt], source.[px], source.[DefaultVal]);
