6f8a40d4151b0c02e7b79607b3e2423bf6b8a812
tyz <yunzheng.tan@newtouch.com>
2025-03-28 18:50:24 +0800

    菜单

---
+++ SqlServer/V1.9/功能菜单.sql
@@ -0,0 +1,8 @@
+
+
+--insert into NewtouchHIS_Base.[dbo].[Sys_Module](Id,ParentId,name,UrlAddress,Target,px,CreateTime,CreatorCode,zt)
+--select NEWID(),'1','基础信息公共库','/CommonLibrary/Index','iframe','5',GETDATE(),'admin','1'
+--insert into NewtouchHIS_Sett.[dbo].[Sys_Module](Id,ParentId,name,UrlAddress,Target,px,CreateTime,CreatorCode,zt)
+--select NEWID(),'1','监管平台信息查询','/JGManage/JGTradUpload/Index','iframe','5',GETDATE(),'admin','1'
+--insert into NewtouchHIS_Sett.[dbo].[Sys_Module](Id,ParentId,name,UrlAddress,Target,px,CreateTime,CreatorCode,zt)
+--select NEWID(),'1','医保进销存查询','/DRGManage/DRGTradUpload/Index','iframe','5',GETDATE(),'admin','1'
